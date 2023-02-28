8516f3e9b7fb2e731eb9659185f7b7a9537c0635
Jonathan-Silva-d-Souza <jonathansilvadesouza2003@gmail.com>
2023-02-28 10:53:19 -0300

    Atividade do dia 2 seção 6 concluida

--- modulo-1-fundamentos/secao-6_Introducao-a-JavaScript-ES6-e-testes-unitarios/dia-2_Primeiros-passos-em-Jest/hydrate.test.js
+++ modulo-1-fundamentos/secao-6_Introducao-a-JavaScript-ES6-e-testes-unitarios/dia-2_Primeiros-passos-em-Jest/hydrate.test.js
@@ -1,2 +1,17 @@
 const hydrate = require('./hydrate.js');
 
+describe('Testa a função hydrate', () => {
+  it('Testa se hydrate é definida', () => {
+    expect(hydrate).toBeDefined();
+  });
+
+  it('Testa se hydrate é uma função', () => {
+    expect(typeof hydrate).toBe('function');
+  });
+
+  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
+    expect(hydrate('1 cerveja')).toBe('1 copo de água');
+    expect(hydrate('1 cachaça, 5 cervejas e 3 copos de vinho')).toBe('9 copos de água');
+    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
+  });
+});
