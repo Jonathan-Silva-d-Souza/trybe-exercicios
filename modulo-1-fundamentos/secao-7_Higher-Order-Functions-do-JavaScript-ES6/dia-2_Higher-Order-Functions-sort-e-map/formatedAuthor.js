7e59ca8c2822fef45c7225266b67aaa8447694c4
Jonathan-Silva-d-Souza <jonathansilvadesouza2003@gmail.com>
2023-03-07 16:05:29 -0300

    Atividade do dia 2 seção 7 concluido

--- modulo-1-fundamentos/secao-7_Higher-Order-Functions-do-JavaScript-ES6/dia-2_Higher-Order-Functions-sort-e-map/formatedAuthor.js
+++ modulo-1-fundamentos/secao-7_Higher-Order-Functions-do-JavaScript-ES6/dia-2_Higher-Order-Functions-sort-e-map/formatedAuthor.js
@@ -3,4 +3,5 @@ const books = require('./data/books');
 const formatedAuthorNamesBirth = () =>
   books.map((book) => `${book.author.name} - ${book.author.birthYear}`);
 
+  
 module.exports = { formatedAuthorNamesBirth };
